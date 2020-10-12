37895b57ea4a6ba03b522f413821d5ed12ffd1a6
Artyom <artyom.arsalanov@gmail.com>
2020-10-13 01:30:52 +0800

    v1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Факториал!---");
+            Console.WriteLine("Введите число:");
+            ulong num = GetNumber();
+            Console.WriteLine($"Факториал от {num} равен {CalculateFactorial(num)}");
+            Console.WriteLine("╔═╗");
+
+        }
+        //метод ввода
+        static ulong GetNumber()
+        {
+            ulong num;
+            bool isNumberCorrect=false;
+            do
+            {
+                string str=Console.ReadLine();
+                isNumberCorrect = UInt64.TryParse(str, out num);
+            } while (!isNumberCorrect&&num>=0);
+            return num;
+        }
+        //метод вычисления факториала
+        static ulong CalculateFactorial(ulong num)
+        {
+            if (num > 1) return num * CalculateFactorial(num - 1);
+            else return 1;
+        }
+
+
+    }
+}
