0d8c945e6212bcf0a36ccca44e4a61945769b3f7
Artyom <artyom.arsalanov@gmail.com>
2020-10-16 01:29:58 +0800

    v2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Lab5
 {
@@ -9,20 +10,42 @@ namespace Lab5
             Console.WriteLine("---Факториал!---");
             Console.WriteLine("Введите число:");
             ulong num = GetNumber();
-            Console.WriteLine($"Факториал от {num} равен {CalculateFactorial(num)}");
-            Console.WriteLine("╔═╗");
+            string factorial = CalculateFactorial(num).ToString();
+            int length = factorial.ToString().Length;
+            length += 2;
+            //╔ ═ ╗ ╣ ╠ ╝ ╩ ╚ ╬ ║
+            while (true)
+            {
+                Console.Clear();
+                Console.ResetColor();
+                Console.Clear();
+                WriteBoxLine(length, '╔', '═', '╗');
+                WriteBoxLine(length, '║', ' ', '║', $"!{num}");
+                WriteBoxLine(length, '╠', '═', '╣');
+                WriteBoxLine(length, '║', ' ', '║', factorial);
+                WriteBoxLine(length, '╚', '═', '╝');
+                Thread.Sleep(1000);
 
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Black;
+                WriteBoxLine(length, '╔', '═', '╗');
+                WriteBoxLine(length, '║', ' ', '║', $"!{num}");
+                WriteBoxLine(length, '╠', '═', '╣');
+                WriteBoxLine(length, '║', ' ', '║', factorial);
+                WriteBoxLine(length, '╚', '═', '╝');
+                Thread.Sleep(1000);
+            }
         }
         //метод ввода
         static ulong GetNumber()
         {
             ulong num;
-            bool isNumberCorrect=false;
+            string str;
             do
             {
-                string str=Console.ReadLine();
-                isNumberCorrect = UInt64.TryParse(str, out num);
-            } while (!isNumberCorrect&&num>=0);
+                str=Console.ReadLine();
+            } while (!UInt64.TryParse(str, out num) && num>=0);
             return num;
         }
         //метод вычисления факториала
@@ -30,6 +53,23 @@ namespace Lab5
         {
             if (num > 1) return num * CalculateFactorial(num - 1);
             else return 1;
+        }
+        public static void WriteBoxLine(int num, char beginSym, char indentSym, char endSym)
+        {
+            Console.Write(beginSym);
+            for (int i = 0; i < num; i++)
+                Console.Write(indentSym);
+            Console.WriteLine(endSym);
+        }
+        public static void WriteBoxLine(int num, char beginSym, char indentSym, char endSym, string str)
+        {
+            Console.Write(beginSym);
+            Console.Write(indentSym);
+            Console.Write(str);
+            int spaceNum = num - str.Length - 1;
+            for (int i = 0; i < spaceNum; i++)
+                Console.Write(indentSym);
+            Console.WriteLine(endSym);
         }
 
 
